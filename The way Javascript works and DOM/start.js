466bf86a0530989cc648da4362661a96caceeb83
MirzaAyon <ayonmirza98@gmail.com>
2022-06-12 18:14:29 +0600

    js internal external file added

--- The way Javascript works and DOM/start.js
+++ The way Javascript works and DOM/start.js
@@ -1,1 +1,5 @@
 Console.log('start');
+
+// querySelector, querySelectorAll, getElementById, getElementsByClassName আর সাথে সাথে আছে node, node type, nodelist, htmlcollection, setAttribute
+
+//discussion topics
